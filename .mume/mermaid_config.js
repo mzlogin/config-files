49d5484579d3cc786186fb1084727ea591496aa0
Zhuang Ma <chumpma@gmail.com>
2023-10-07 14:09:59 +0800

    compatible with vscode plugin markdown-preview-enhanced

--- .mume/mermaid_config.js
+++
@@ -1,7 +0,0 @@
-// config mermaid init call
-// https://github.com/mermaid-js/mermaid/blob/master/docs/config/setup/modules/mermaidAPI.md#mermaidapi-configuration-defaults
-//
-// You can edit the 'MERMAID_CONFIG' variable below.
-MERMAID_CONFIG = {
-  startOnLoad: false
-}
